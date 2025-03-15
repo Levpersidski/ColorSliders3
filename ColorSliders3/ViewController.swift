9b43cbe49e576c27759e8f85b2bb9d779d65a33d
Levpersidski <amor180888@mail.ru>
2025-03-15 15:40:03 +0300

    Initial Commit

---
+++ ColorSliders3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSliders3
+//
+//  Created by SWIFT on 15.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
