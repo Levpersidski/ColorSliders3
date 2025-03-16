7b5ba8626e866e54ad737a182efc9539e2c73283
Levpersidski <amor180888@mail.ru>
2025-03-16 21:18:49 +0300

    added two methods and changed valueLabels constraints

--- ColorSliders3/ViewController.swift
+++ ColorSliders3/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateColors()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        updateLabels()
     }
-
-
+    
+    @IBAction func changeRedSlider(_ sender: Any) {
+        updateColors()
+        updateLabels()
+    }
+    
+    @IBAction func changeGreenSlider(_ sender: Any) {
+        updateColors()
+        updateLabels()
+    }
+    
+    @IBAction func changeBlueSlider(_ sender: Any) {
+        updateColors()
+        updateLabels()
+    }
+    
+    func updateColors() {
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        colorView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
+    func updateLabels() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
+
 
